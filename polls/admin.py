21346d75424b03e169b1aad9383cb78e4cf3a5b1
ZvonarevEgor <zvonarev96@mail.ru>
2019-08-30 23:17:33 +0300

    finish seventh part of tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
+from django.utils import timezone
+from .models import Choice, Question
+import datetime
 
-from .models import Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+
+admin.site.register(Question, QuestionAdmin)
+
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.core.urlresolvers import reverse
 from .models import Choice, Question
 from django.views import generic
 from django.utils import timezone
